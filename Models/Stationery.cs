7cbe8c57afa3de6d6faf32c9f27bd654903330d9
BiancaZYCao <45491654+BiancaZYCao@users.noreply.github.com>
2020-08-18 01:00:25 +0800

    upload missing files stationery

---
+++ Models/Stationery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//This class is used to test connection with front end. -Bianca
+
+namespace BackEndAD.Models
+{
+    public class Stationery
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int quantity { get; set; }
+    }
+}
