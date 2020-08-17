6d6e33ca6adcdcb8f2a87e93baf281d952ad13af
BiancaZYCao <45491654+BiancaZYCao@users.noreply.github.com>
2020-08-18 00:53:13 +0800

    upload missing file reqDetail

---
+++ Models/RequisitonDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndAD.Models
+{
+    public class RequisitionDetail
+    {
+        public int Id { get; set; }
+
+        public int reqQty { get; set; }
+        public string status { get; set; }
+
+        public int RequisitionId { get; set; }
+        public virtual Requisition Requisition { get; set; }
+        public int StationeryId { get; set; }
+        public virtual Stationery Stationery { get; set; }
+    }
+}
