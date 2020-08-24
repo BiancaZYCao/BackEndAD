d53f114b1f23de841a4109e9d0ef87a1290c20f3
Martin Ng <nghongyi125@hotmail.com>
2020-08-24 21:46:09 +0800

    retrieval now creates: disbursement disbursement detail stock adjustment stock adjustment detail;
    and updates:
    requisistion detail
    requisition
    stationery item

--- ServiceImpl/StoreClerkImpl.cs
+++ ServiceImpl/StoreClerkImpl.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
 
 namespace BackEndAD.ServiceImpl
 {
@@ -396,6 +397,30 @@ namespace BackEndAD.ServiceImpl
         public void updateStationery(Stationery s)
         {
             unitOfWork.GetRepository<Stationery>().Update(s);
+            unitOfWork.SaveChanges();
+        }
+
+        public Task<IList<CollectionInfo>> findAllCollectionPointAsync()
+        {
+            return unitOfWork.GetRepository<CollectionInfo>().GetAllAsync() ;
+        }
+
+        public void saveDisbursementList(DisbursementList newDL)
+        {
+            unitOfWork.GetRepository<DisbursementList>().Insert(newDL);
+            unitOfWork.SaveChanges();
+        }
+
+        public void saveDisbursementDetail(DisbursementDetail currDB)
+        {
+            unitOfWork.GetRepository<DisbursementDetail>().Insert(currDB);
+            unitOfWork.SaveChanges();
+        }
+
+        public void udpateRequisitionDetail(RequisitionDetail rd)
+        {
+            unitOfWork.GetRepository<RequisitionDetail>().Update(rd);
+            unitOfWork.SaveChanges();
         }
     }
 }
